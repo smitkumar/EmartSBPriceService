5a1e207be8209a01975e4eba19edaad60c647322
unknown <sku281@BLRSKU28163518.sapient.com>
2016-01-22 13:12:06 +0530

    added project structure.

---
+++ src/main/java/com/smit/emart/emartproductservice/App.java
@@ -0,0 +1,13 @@
+package com.smit.emart.emartproductservice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
